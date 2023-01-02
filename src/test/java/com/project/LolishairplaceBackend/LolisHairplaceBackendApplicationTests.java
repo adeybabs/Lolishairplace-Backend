8295e55a7d4797efe73169404fad3bcb7de59d25
adeybabs <adeybabs.aa@gmail.com>
2023-01-02 23:16:20 +0100

    first commit

---
+++ src/test/java/com/project/LolishairplaceBackend/LolisHairplaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.LolishairplaceBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolisHairplaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
