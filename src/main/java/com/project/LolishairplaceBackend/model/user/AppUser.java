f318e9d339390d0fe3b085641af73be751f35db7
adeybabs <adeybabs.aa@gmail.com>
2023-01-16 12:45:25 +0100

    web security config

--- src/main/java/com/project/LolishairplaceBackend/model/user/AppUser.java
+++ src/main/java/com/project/LolishairplaceBackend/model/user/AppUser.java
@@ -7,9 +7,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.*;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @PasswordValueMatch.List({
         @PasswordValueMatch(
@@ -21,11 +26,10 @@ import jakarta.persistence.*;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @NoArgsConstructor
+@EqualsAndHashCode
 @Entity
 @ToString
-@AllArgsConstructor
 public class AppUser implements UserDetails {
 
     @SequenceGenerator(
@@ -37,9 +41,8 @@ public class AppUser implements UserDetails {
     @Id
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "student_sequence"
+            generator = "user_sequence"
     )
-
     private Long id;
     @NonNull
     @NotBlank(message = "Name is mandatory")
@@ -65,4 +68,61 @@ public class AppUser implements UserDetails {
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
 
+    private Boolean locked = false;
+    private Boolean enabled = false;
+
+    public AppUser(@NonNull String firstName, String lastName, String email, @NonNull String password, @NonNull String confirmPassword, AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.appUserRole = appUserRole;
+
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority =
+                new SimpleGrantedAuthority(appUserRole.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
---
+++ src/main/java/com/project/LolishairplaceBackend/service/ConfirmationTokenService.java
@@ -0,0 +1,32 @@
+package com.project.LolishairplaceBackend.service;
+
+import com.project.LolishairplaceBackend.model.token.ConfirmationToken;
+import com.project.LolishairplaceBackend.repository.ConfirmationTokenRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class ConfirmationTokenService {
+
+    //expose the ability to save a confirmation token
+
+    private final ConfirmationTokenRepo confirmationTokenRepo;
+
+    public void saveConfirmationToken(ConfirmationToken token){
+        confirmationTokenRepo.save(token);
+    }
+
+    public Optional<ConfirmationToken> getToken(String token) {
+
+        return confirmationTokenRepo.findByToken(token);
+    }
+
+    public int setConfirmedAt(String token) {
+        return confirmationTokenRepo.updateConfirmedAt(
+                token, LocalDateTime.now());
+    }
+}
